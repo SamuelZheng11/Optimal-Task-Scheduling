d6448499215dfcc349796ef37f1988e29b8248e4
Edwar Zhang <Edwar.zhang1@gmail.com>
2018-07-30 17:26:30 +1200

    added graphstream and can successfully parse dot file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,19 +5,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.stream.file.FileSource;
+import org.graphstream.stream.file.FileSourceDOT;
+import org.graphstream.stream.file.FileSourceFactory;
+import org.graphstream.stream.file.dot.DOTParser;
+
+import java.io.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        String filePath = "Input/example-input-graphs/Nodes_7_OutTree.dot";
+        try {
+            FileSource fs = new FileSourceDOT();
+            Graph g = new DefaultGraph("Graph");
+            fs.addSink(g);
+            fs.readAll(filePath);
+            g.display();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
