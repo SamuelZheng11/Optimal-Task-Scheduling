0d0bbce3c566cc5c391ce302f3a81231969ddeae
Alex Morgan <alexrmorgan@outlook.com>
2018-07-31 16:37:58 +1200

    pseudocode-based recursive function - Alex Morgan
    This code still requires the free tasks and state adding functions in order to work. Also I think it can be improved by removing current/previous task and processor, but I wanted the pure version on a commit before I tamper with it.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,10 @@
 package main;
 
+import common.DependencyGraph;
+import common.State;
+import common.TaskDependencyNode;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import common.DependencyGraph;
 
 public class Main extends Application {
     private DependencyGraph dg = DependencyGraph.getGraph();
@@ -17,4 +19,33 @@ public class Main extends Application {
         launch(args);
     }
 
+    public State recursion(TaskDependencyNode currentTask, int currentProc, TaskDependencyNode previousTask,
+                           int previousProc, int numProc, int depth, State state, State bestFoundState,
+                           int numTasks){
+        TaskDependencyNode[] freeTasks = free(state);
+        if (freeTasks.length>0){
+            for (int i = 0; i < freeTasks.length; i++) {
+                for (int j = 0; j < numProc; j++) {
+                    depth++;
+                    previousTask = currentTask;
+                    previousProc =currentProc;
+                    currentTask = freeTasks[i];
+                    currentProc = j;
+                    State newState = state.add(currentTask, currentProc);
+                    if (newState.getHeuristicValue()<= bestFoundState.getHeuristicValue() && depth == numTasks){
+                        bestFoundState = newState;
+                    }
+                    else if (newState.getHeuristicValue() <= bestFoundState.getHeuristicValue() && depth < numTasks) {
+                        State foundState = recursion(currentTask, currentProc, previousTask, previousProc, numProc, depth,
+                                newState, bestFoundState, numTasks);
+                        if (foundState.getHeuristicValue() <= bestFoundState.getHeuristicValue()) {
+                            bestFoundState = foundState;
+                        }
+                    }
+                    depth--;
+                }
+            }
+            return bestFoundState;
+        }
+    }
 }
