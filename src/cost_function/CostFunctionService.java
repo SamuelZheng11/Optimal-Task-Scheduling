41d591f28c51cd8a238ad88c14084c41e17f83ab
Sam <samuelzheng11@gmail.com>
2018-08-02 10:14:40 +1200

    add files back

---
+++ src/cost_function/CostFunctionService.java
@@ -0,0 +1,164 @@
+package cost_function;
+
+import common.*;
+import exception_classes.CostFunctionException;
+import javafx.concurrent.Task;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class CostFunctionService {
+    private static final int FIRST_INDEX = 0;
+    private State _state;
+    private int inputProcessorCompletionTime = 0;
+    private int heuristicSum = 0;
+
+    public State scheduleNode(TaskDependencyNode node, Job[] onProcessor, State withCurrentState, int costOfAllNodes) {
+
+        _state = withCurrentState;
+        List<Job> processor = new ArrayList<>(Arrays.asList(onProcessor));
+        // identify what processor the caller has send us
+        int processorNumber = 0;
+        if (onProcessor.length > 0) {
+            processorNumber = this.identifyProcessor(onProcessor[FIRST_INDEX]);
+        } else {
+            processorNumber = this.identifyProcessor(null);
+        }
+
+        ArrayList<TaskDependencyNode> parentNodes = new ArrayList<>();
+        ArrayList<Integer> parentCommDelayEdges = new ArrayList<>();
+        for(int i = 0; i < node._parents.length; i++){
+            parentNodes.add(node._parents[i]._parent);
+            parentCommDelayEdges.add(node._parents[i]._communicationDelay);
+        }
+
+        // check if the current processor contains any parent jobs
+        boolean parentOnProcessor = isParentOnProcessor(onProcessor, parentNodes);
+
+        // check if the node has any parents
+        if (parentNodes.size() == 0) {
+            // returns the state with the new task on the processor
+            processor.add(new TaskJob(node._duration, node._name, node));
+            _state.getJobLists()[processorNumber] = processor.toArray(new Job[processor.size()]);
+            _state.getJobListDuration()[processorNumber] += node._duration;
+            return new State(_state.getJobLists(), _state.getJobListDuration(), costOfAllNodes - heuristicSum - node._duration);
+        }
+
+        // otherwise attempt to find the parent with the lowest communication time
+        int lowestCostToSchedule;
+        if(!parentOnProcessor) {
+            lowestCostToSchedule = this.calculateCostToSchedule(parentNodes, parentCommDelayEdges, processorNumber);
+        } else {
+            lowestCostToSchedule = 0;
+        }
+
+        // check that the lowest communication time is not infinity
+        if (lowestCostToSchedule == Integer.MAX_VALUE){
+            throw new CostFunctionException("Lowest Communication Time have value infinity");
+        }
+
+        // add to processor with communication delay
+        if(!parentOnProcessor && lowestCostToSchedule > this.inputProcessorCompletionTime) {
+            processor.add(new DelayJob(lowestCostToSchedule - this.inputProcessorCompletionTime));
+            _state.getJobListDuration()[processorNumber] += lowestCostToSchedule - this.inputProcessorCompletionTime;
+            this.heuristicSum += (lowestCostToSchedule - this.inputProcessorCompletionTime);
+        }
+        processor.add(new TaskJob(lowestCostToSchedule, node._name, node));
+        _state.getJobLists()[processorNumber] = processor.toArray(new Job[processor.size()]);
+        _state.getJobListDuration()[processorNumber] += node._duration;
+
+        return new State(_state.getJobLists(), _state.getJobListDuration(), costOfAllNodes - heuristicSum - node._duration);
+    }
+
+    private int calculateCostToSchedule(ArrayList<TaskDependencyNode> parents, ArrayList<Integer> commDealy, int skipProcessorNumber) {
+
+        // find the parent's completion time on other processors
+        List<Integer> buildingCostForProcessor = new ArrayList<>(Collections.nCopies(_state.getJobLists().length, 0));
+        List<Integer> currentBestForProcessor = new ArrayList<>(Collections.nCopies(_state.getJobLists().length, 0));
+        List<Boolean> parentFound = new ArrayList<>(Collections.nCopies(_state.getJobLists().length, Boolean.FALSE));
+
+        for (int i = 0; i < _state.getJobLists().length; i++){
+
+            // skip the row if it has already been check by the outer method
+            if(skipProcessorNumber == i) {
+                continue;
+            }
+
+            currentBestForProcessor.set(i, Integer.MAX_VALUE);
+
+            for (int j = 0; j < _state.getJobLists()[i].length; j++){
+                buildingCostForProcessor.set(i, buildingCostForProcessor.get(i) + 1);
+
+                if (_state.getJobLists()[i][j].getClass() == TaskJob.class) {
+                    TaskJob potentialParentJob = (TaskJob) _state.getJobLists()[i][j];
+
+                    if (parents.contains(potentialParentJob.getNode())) {
+                        if (currentBestForProcessor.get(i) > buildingCostForProcessor.get(i) + commDealy.get(parents.indexOf(potentialParentJob.getNode()))) {
+                            currentBestForProcessor.set(i, buildingCostForProcessor.get(i) + commDealy.get(parents.indexOf(potentialParentJob.getNode())));
+                            parentFound.set(i, true);
+                        }
+                    }
+                }
+            }
+        }
+
+        // calculate best cost to schedule with
+        int lowestCost = Integer.MAX_VALUE;
+        for(int i = 0; i < currentBestForProcessor.size(); i++){
+            // skip the row if it has already been check by the outer method
+            if(skipProcessorNumber == i) {
+                continue;
+            }
+
+            // get the lowest cost out of all the processors
+            if(parentFound.get(i) == true && currentBestForProcessor.get(i) < lowestCost){
+                lowestCost = currentBestForProcessor.get(i);
+
+            }
+        }
+
+        // calculate the current heuristic cost
+        for(int i = 0; i < buildingCostForProcessor.size(); i++) {
+            this.heuristicSum += buildingCostForProcessor.get(i);
+        }
+
+        return lowestCost;
+    }
+
+    private boolean isParentOnProcessor(Job[] onProcessor, ArrayList<TaskDependencyNode> parentNodes) {
+        boolean result = false;
+        for (int i = 0; i < onProcessor.length; i++) {
+            this.heuristicSum += onProcessor[i].getDuration();
+            this.inputProcessorCompletionTime += onProcessor[i].getDuration();
+            for (int j = 0; j < parentNodes.size(); j++) {
+                if(onProcessor[i] instanceof TaskJob) {
+                    TaskJob job = (TaskJob) onProcessor[i];
+                    if (job.getNode() == parentNodes.get(j)) {
+                        result = true;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private int identifyProcessor(Job firstJobOnProcessor) {
+
+        boolean enableShortCircuit = (firstJobOnProcessor == null);
+        for (int i = 0; i < _state.getJobLists().length; i++){
+            if (enableShortCircuit && _state.getJobLists()[i].length == 0) {
+                // if the first index is null and we want to find a processor with no tasks on it then return the processor number
+                return i;
+            } else if (_state.getJobLists()[FIRST_INDEX][i] == firstJobOnProcessor) {
+                // otherwise try to identify the processor number
+                return i;
+            }
+        }
+
+        throw new CostFunctionException("Index of processor was not found");
+    }
+}
---
+++ src/unit_tests/CostFunctionServiceTest.java
@@ -0,0 +1,197 @@
+package unit_tests;
+
+import common.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import cost_function.CostFunctionService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CostFunctionServiceTest {
+
+    State currentState;
+    Job[] targetProcessor;
+    List<TaskDependencyNode> _nodes = new ArrayList<>();
+    final static int SUM_OF_ALL_NODES = 28;
+
+    @Before
+    public void setUp() throws Exception {
+        this.targetProcessor = new TaskJob[] {};
+        Job[][] jobList = new Job[][]{{},{}};
+        this.currentState = new State(jobList, new int[]{0,0}, 0);
+
+        TaskDependencyNode rootNode = new TaskDependencyNode(1, new TaskDependencyEdge[]{}, new TaskDependencyEdge[]{}, "root node");
+        TaskDependencyNode node1 = new TaskDependencyNode(2, new TaskDependencyEdge[]{}, new TaskDependencyEdge[]{}, "Level 1 node");
+        TaskDependencyNode node2 = new TaskDependencyNode(3, new TaskDependencyEdge[]{}, new TaskDependencyEdge[]{}, "level 2 node 1");
+        TaskDependencyNode node3 = new TaskDependencyNode(4, new TaskDependencyEdge[]{}, new TaskDependencyEdge[]{}, "level 2 node 2");
+        TaskDependencyNode node4 = new TaskDependencyNode(5, new TaskDependencyEdge[]{}, new TaskDependencyEdge[]{}, "level 3 node");
+        TaskDependencyNode node5 = new TaskDependencyNode(6, new TaskDependencyEdge[]{}, new TaskDependencyEdge[]{}, "level 4 node");
+        TaskDependencyNode isolatedNode = new TaskDependencyNode(7, new TaskDependencyEdge[]{}, new TaskDependencyEdge[]{}, "level 3 node");
+
+        _nodes.add(rootNode);
+        _nodes.add(node1);
+        _nodes.add(node2);
+        _nodes.add(node3);
+        _nodes.add(node4);
+        _nodes.add(node5);
+        _nodes.add(isolatedNode);
+
+        rootNode._children = new TaskDependencyEdge[]{new TaskDependencyEdge(rootNode, node1, 1)};
+        node1._parents = new TaskDependencyEdge[]{new TaskDependencyEdge(rootNode, rootNode, 1)};
+        node1._children = new TaskDependencyEdge[]{new TaskDependencyEdge(node1, node2, 1), new TaskDependencyEdge(node1, node3, 1)};
+        node2._parents = new TaskDependencyEdge[]{new TaskDependencyEdge(node1, node2, 1)};
+        node2._children = new TaskDependencyEdge[]{new TaskDependencyEdge(node2, node3, 1), new TaskDependencyEdge(node2, node4, 1)};
+        node3._parents = new TaskDependencyEdge[]{new TaskDependencyEdge(node1, node3, 1),new TaskDependencyEdge(node2, node3, 1)};
+        node4._parents = new TaskDependencyEdge[]{new TaskDependencyEdge(node2, node4, 1)};
+        node4._children = new TaskDependencyEdge[]{new TaskDependencyEdge(node4, node5, 1)};
+        node5._parents = new TaskDependencyEdge[]{new TaskDependencyEdge(node4, node5, 1)};
+    }
+
+    @After
+    public void cleanUp() {
+        this.currentState = null;
+        this.targetProcessor = null;
+        this._nodes = null;
+    }
+
+    @Test
+    public void shouldScheduleFirstOrIsolatedJob() {
+        this.targetProcessor = new TaskJob[] {};
+        Job[][] jobList = new Job[][]{this.targetProcessor};
+        this.currentState = new State(jobList, new int[]{0}, 0);
+
+        State result = new CostFunctionService().scheduleNode(_nodes.get(0), this.targetProcessor, this.currentState, this.SUM_OF_ALL_NODES);
+
+        // check that the Job scheduled is a TaskJob
+        try{
+            TaskJob scheduledJob =(TaskJob) result.getJobLists()[0][0];
+
+            // check that the scheduled TaskJob is the root node
+            Assert.assertEquals(scheduledJob.getNode(), _nodes.get(0));
+            Assert.assertEquals(result.getJobListDuration()[0], 1);
+            Assert.assertEquals(result.getHeuristicValue(), 27);
+        } catch (ClassCastException cce) {
+            Assert.fail();
+        }
+
+        this.targetProcessor = new TaskJob[] {};
+        jobList = new Job[][]{this.targetProcessor};
+        this.currentState = new State(jobList, new int[]{0}, 0);
+
+        result = new CostFunctionService().scheduleNode(_nodes.get(6), this.targetProcessor, this.currentState, this.SUM_OF_ALL_NODES);
+
+        // check that the Job scheduled is a TaskJob
+        try{
+            TaskJob scheduledJob =(TaskJob) result.getJobLists()[0][0];
+
+            // check that the scheduled TaskJob is the root node
+            Assert.assertEquals(scheduledJob.getNode(), _nodes.get(6));
+            Assert.assertEquals(result.getJobListDuration()[0], _nodes.get(6)._duration);
+            Assert.assertEquals(result.getHeuristicValue(), this.SUM_OF_ALL_NODES - _nodes.get(6)._duration);
+        } catch (ClassCastException cce) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void shouldReturnTargetProcessorWithNoDelay() {
+
+        TaskJob rootNodeTask = new TaskJob(_nodes.get(0)._duration, _nodes.get(0)._name, _nodes.get(0));
+
+        this.targetProcessor = new TaskJob[] {rootNodeTask};
+        Job[][] jobList = new Job[][]{this.targetProcessor};
+        this.currentState = new State(jobList, new int[]{0}, 0);
+
+        this.currentState.getJobLists()[0][0] = rootNodeTask;
+        this.currentState.getJobListDuration()[0] += rootNodeTask.getDuration();
+
+        State result = new CostFunctionService().scheduleNode(_nodes.get(1), this.targetProcessor, this.currentState, this.SUM_OF_ALL_NODES);
+
+        // check that the Job scheduled is a TaskJob
+        try{
+            TaskJob scheduledJob = (TaskJob) result.getJobLists()[0][1];
+
+            // check that the scheduled TaskJob is the root node
+            Assert.assertEquals(scheduledJob.getNode(), _nodes.get(1));
+            Assert.assertEquals(result.getJobListDuration()[0], 3);
+            Assert.assertEquals(result.getHeuristicValue(), 25);
+        } catch (ClassCastException cce) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void shouldScheduleNodeWithDelay() {
+
+        Job rootJob = new TaskJob(1, _nodes.get(0)._name, _nodes.get(0));
+
+        this.targetProcessor = new Job[] {};
+        Job[] secondaryProcessor = new Job[] {rootJob};
+        this.currentState.getJobLists()[0] = this.targetProcessor;
+        this.currentState.getJobLists()[1] = secondaryProcessor;
+
+        this.currentState.getJobListDuration()[1] += ((TaskJob) rootJob).getNode()._duration;
+
+        State result = new CostFunctionService().scheduleNode(_nodes.get(1), this.targetProcessor, this.currentState, this.SUM_OF_ALL_NODES);
+
+        // check that the Job scheduled is a TaskJob
+        try{
+
+            TaskJob scheduledJob =(TaskJob) result.getJobLists()[0][1];
+
+            // check that the scheduled TaskJob is the root node
+            Assert.assertEquals(scheduledJob.getNode(), _nodes.get(1));
+
+            Assert.assertEquals(result.getJobListDuration()[0], 4);
+            Assert.assertEquals(result.getJobListDuration()[1], 1);
+            Assert.assertEquals(result.getHeuristicValue(), 23);
+        } catch (ClassCastException cce) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void shouldScheduleLastJob() {
+
+        Job rootJob = new TaskJob(1, _nodes.get(0)._name, _nodes.get(0));
+        Job job1 = new TaskJob(1, _nodes.get(1)._name, _nodes.get(1));
+        Job job2 = new TaskJob(1, _nodes.get(2)._name, _nodes.get(2));
+        Job job3 = new TaskJob(1, _nodes.get(3)._name, _nodes.get(3));
+        Job job4 = new TaskJob(1, _nodes.get(4)._name, _nodes.get(4));
+
+        this.targetProcessor = new Job[] {rootJob, job1, job2, job3, job4};
+        this.currentState.getJobLists()[0] = this.targetProcessor;
+
+        this.currentState.getJobListDuration()[0] += ((TaskJob) rootJob).getNode()._duration;
+        this.currentState.getJobListDuration()[0] += ((TaskJob) job1).getNode()._duration;
+        this.currentState.getJobListDuration()[0] += ((TaskJob) job2).getNode()._duration;
+        this.currentState.getJobListDuration()[0] += ((TaskJob) job3).getNode()._duration;
+        this.currentState.getJobListDuration()[0] += ((TaskJob) job4).getNode()._duration;
+
+
+        State result = new CostFunctionService().scheduleNode(_nodes.get(5), this.targetProcessor, this.currentState, this.SUM_OF_ALL_NODES);
+
+        // check that the Job scheduled is a TaskJob
+        try{
+            TaskJob scheduledJob =(TaskJob) result.getJobLists()[0][5];
+
+            // check that the scheduled TaskJob is the root node
+            Assert.assertEquals(scheduledJob.getNode(), _nodes.get(5));
+            int p1FinishTime = 0;
+            for(int i = 0; i < _nodes.size()-1; i++) {
+                p1FinishTime += _nodes.get(i)._duration;
+            }
+            Assert.assertEquals(result.getJobListDuration()[0], p1FinishTime);
+        } catch (ClassCastException cce) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void shouldReturnCorrectHeuristic() {
+
+    }
+}
