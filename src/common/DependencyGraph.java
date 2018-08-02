913bf4f2ef15481d92be89d0eb7a11fa2fe58286
Edwar Zhang <Edwar.zhang1@gmail.com>
2018-08-02 21:26:39 +1200

    input parsing using graphstream

--- src/common/DependencyGraph.java
+++ src/common/DependencyGraph.java
@@ -1,5 +1,14 @@
 package common;
 
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.AbstractEdge;
+import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.stream.file.FileSource;
+import org.graphstream.stream.file.FileSourceDOT;
+import org.graphstream.stream.file.FileSourceFactory;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -38,50 +47,74 @@ public class DependencyGraph {
     //    A edge always contains the '-' character as a part of the arrow symbol
     //    The first line is always some file header containing the word "{"
     //    The last line prior to null contains "}"
-    public void parse() {
-        Pattern WEIGHTPATTERN = Pattern.compile("Weight=(\\d+)");
-        Pattern NODENAMEPATTERN = Pattern.compile("\t(\\d+)\t");
-        Pattern EDGESOURCEPATTERN = Pattern.compile("\t(\\d+) ");
-        Pattern EDGEDESTPATTERN = Pattern.compile(" (\\d+)\t");
 
-        Matcher _weightMatcher;
-        Matcher _nodeNameMatcher;
-        Matcher _edgeSourcePatternMatcher;
-        Matcher _edgeDestPatternMatcher;
-
-        TaskDependencyNode previous;
+    public void parse(){
+        String filePath = "Input/example-input-graphs/Nodes_7_OutTree.dot";
+        Graph g = new DefaultGraph("g");
+        FileSource fs = new FileSourceDOT();
+        fs.addSink(g);
         try {
-            String filePath = "Input/example-input-graphs/Nodes_9_SeriesParallel.dot";
-            BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
-            String s = br.readLine();
-            while (s != null) {
-                if (s.contains("{") || s.contains("}") ) {
-                }else if(s.contains("-")){
-                    _edgeSourcePatternMatcher = EDGESOURCEPATTERN.matcher(s);
-                    _edgeSourcePatternMatcher.find();
-                    _edgeDestPatternMatcher = EDGEDESTPATTERN.matcher(s);
-                    _edgeDestPatternMatcher.find();
-                    _weightMatcher = WEIGHTPATTERN.matcher(s);
-                    _weightMatcher.find();
-                    _nodes.get(_edgeDestPatternMatcher.group(1))._parents.add(new TaskDependencyEdge(_nodes.get(_edgeSourcePatternMatcher.group(1)),_nodes.get(_edgeDestPatternMatcher.group(1)),Integer.valueOf(_weightMatcher.group(1))));
-                    _nodes.get(_edgeSourcePatternMatcher.group(1))._children.add(new TaskDependencyEdge(_nodes.get(_edgeSourcePatternMatcher.group(1)),_nodes.get(_edgeDestPatternMatcher.group(1)),Integer.valueOf(_weightMatcher.group(1))));
-                }else {
-                    _nodeNameMatcher = NODENAMEPATTERN.matcher(s);
-                    _nodeNameMatcher.find();
-                    _weightMatcher = WEIGHTPATTERN.matcher(s);
-                    _weightMatcher.find();
-                    previous = new TaskDependencyNode(Integer.valueOf(_weightMatcher.group(1)), _nodeNameMatcher.group(1));
-                    _nodes.put(previous._name,previous);
-                }
-                s = br.readLine();
+            fs.readAll(filePath);
+            Iterator<Node> iter = g.getNodeIterator();
+            while(iter.hasNext()){
+                 Node test = iter.next();
+                 Iterator<Edge> iter2 = test.getEdgeIterator();
+                 while(iter2.hasNext()){
+                     Edge edge = iter2.next();
+                     edge.addAttribute("delay", new Integer(10));
+                     System.out.println(edge.getAttribute("delay") + "");
+                 }
             }
-            for (Map.Entry<String, TaskDependencyNode> entry : _nodes.entrySet())
-            {
-                System.out.println(entry.getKey() + "/" + entry.getValue());
-            }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
+
+//    public void parse() {
+//        Pattern WEIGHTPATTERN = Pattern.compile("Weight=(\\d+)");
+//        Pattern NODENAMEPATTERN = Pattern.compile("\t(\\d+)\t");
+//        Pattern EDGESOURCEPATTERN = Pattern.compile("\t(\\d+) ");
+//        Pattern EDGEDESTPATTERN = Pattern.compile(" (\\d+)\t");
+//
+//        Matcher _weightMatcher;
+//        Matcher _nodeNameMatcher;
+//        Matcher _edgeSourcePatternMatcher;
+//        Matcher _edgeDestPatternMatcher;
+//
+//        TaskDependencyNode previous;
+//        try {
+//            String filePath = "Input/example-input-graphs/Nodes_9_SeriesParallel.dot";
+//            BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
+//            String s = br.readLine();
+//            while (s != null) {
+//                if (s.contains("{") || s.contains("}") ) {
+//                }else if(s.contains("-")){
+//                    _edgeSourcePatternMatcher = EDGESOURCEPATTERN.matcher(s);
+//                    _edgeSourcePatternMatcher.find();
+//                    _edgeDestPatternMatcher = EDGEDESTPATTERN.matcher(s);
+//                    _edgeDestPatternMatcher.find();
+//                    _weightMatcher = WEIGHTPATTERN.matcher(s);
+//                    _weightMatcher.find();
+//                    _nodes.get(_edgeDestPatternMatcher.group(1))._parents.add(new TaskDependencyEdge(_nodes.get(_edgeSourcePatternMatcher.group(1)),_nodes.get(_edgeDestPatternMatcher.group(1)),Integer.valueOf(_weightMatcher.group(1))));
+//                    _nodes.get(_edgeSourcePatternMatcher.group(1))._children.add(new TaskDependencyEdge(_nodes.get(_edgeSourcePatternMatcher.group(1)),_nodes.get(_edgeDestPatternMatcher.group(1)),Integer.valueOf(_weightMatcher.group(1))));
+//                }else {
+//                    _nodeNameMatcher = NODENAMEPATTERN.matcher(s);
+//                    _nodeNameMatcher.find();
+//                    _weightMatcher = WEIGHTPATTERN.matcher(s);
+//                    _weightMatcher.find();
+//                    previous = new TaskDependencyNode(Integer.valueOf(_weightMatcher.group(1)), _nodeNameMatcher.group(1));
+//                    _nodes.put(previous._name,previous);
+//                }
+//                s = br.readLine();
+//            }
+//            for (Map.Entry<String, TaskDependencyNode> entry : _nodes.entrySet())
+//            {
+//                System.out.println(entry.getKey() + "/" + entry.getValue());
+//            }
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+//    }
 
 }
